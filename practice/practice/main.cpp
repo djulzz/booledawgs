19d34651a80cc237f470f4512c08d1b0c30926ad
djulzz <julien.esposito@gmail.com>
2018-09-23 12:54:27 -0700

    Initial Commit for the Programming Competition
    Initial Commit for the Programming Competition

---
+++ practice/practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Julien Esposito on 9/23/18.
+//  Copyright © 2018 Julien Esposito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
